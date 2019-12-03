0ce55934b80cd5aebb24b7a532c91628af7bf5f2
LogosItAcademy <Tester99037@gmail.com>
2019-12-03 10:11:19 +0200

    first commit

---
+++ Java_Advanced_lesson-03/src/main/java/ua/lviv/lgs/Log.java
@@ -0,0 +1,21 @@
+package ua.lviv.lgs;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+public class Log {
+
+	private static Logger LOG = Logger.getLogger(Log.class);
+
+	public static void main(String[] args) {
+
+		DOMConfigurator.configure("log4j.xml");
+
+		LOG.debug("DEBUG Message");
+		LOG.info("INFO Message");
+		LOG.warn("WARN Message");
+		LOG.error("ERROR Message");
+		LOG.fatal("FATAL Message");
+
+	}
+}
